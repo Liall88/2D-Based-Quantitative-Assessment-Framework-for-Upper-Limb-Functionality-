76a50417d6b615b0f7e13e7a7fa5e53ef03bc127
Liall Arafa <la2817@fuhe.doc.ic.ac.uk>
2018-06-04 15:47:36 +0100

    example local outputs

--- src/ArffIO/ArffReader.java
+++ src/ArffIO/ArffReader.java
@@ -29,11 +29,24 @@ public class ArffReader {
  	final static String OUTPUTFOLDER = "/homes/la2817/Desktop/Outputs/arff_Outputs/testData/"+folderName+"/averages/";
  	final static String INPUTFOLDER = INPUTDIR +"test"+trial+"Metrics" + "/";
 	//static File[] metricFiles = new File(INPUTFOLDER).listFiles();
-	static ArrayList <String> str =new ArrayList<String>();
 	
 
 	
-	//ORIGINAL : general method which gets the path for each JSON file and saves it in an array 
+	
+	public void readArff(){
+		
+	}
+		
+	public static void main (String [] args) {
+		System.out.println("DEBUG:"+ INPUTFOLDER);
+		System.out.println("DEBUG:"+ new File(INPUTFOLDER).listFiles());
+		
+		//0 is speeds, 1 is jerkiness, 2 is disFromRef, 3 is angles
+		File[] metricFiles = new File(INPUTFOLDER).listFiles();
+		
+	}
+
+//ORIGINAL : general method which gets the path for each JSON file and saves it in an array 
 		//check if directory and loop through each file and set paths which is used in setSekeltonList() to instantiate skeletons
 		public static void setPathArray(File[] files,ArrayList<String> strList) {
 		    for (File file : files) {
@@ -46,20 +59,5 @@ public class ArffReader {
 		            //System.out.println(file.getPath());
 		        }
 		    }
-		}
-		
-	public void readArff(){
-		
-	}
-		
-	public static void main (String [] args) {
-		//setPathArray(metricFiles,str);
-		System.out.println("DEBUG:"+ INPUTFOLDER);
-		System.out.println("DEBUG:"+ new File(INPUTFOLDER).listFiles());
-		
-		//0 is speeds, 1 is jerkiness, 2 is disFromRef, 3 is angles
-		File[] metricFiles = new File(INPUTFOLDER).listFiles();
-		
-	}
-
+		}	
 }
