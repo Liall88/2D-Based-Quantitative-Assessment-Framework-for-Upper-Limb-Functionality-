79f61a6c6f8422f6bfdff045faeb57eda947e8dc
Liall Arafa <la2817@fuhe.doc.ic.ac.uk>
2018-06-06 16:45:58 +0100

    updated read me

---
+++ src/dataExtraction/Interpolator.java
@@ -0,0 +1,17 @@
+/**
+ * Author: Liall Arafa
+   Imperial College London
+   6 Jun 2018
+2DFuglMeyer
+	
+ */
+package dataExtraction;
+
+/**
+ * @author la2817
+ *
+ *This class interpolates multiple Trials of openPose data captured
+ */
+public class Interpolator {
+
+}
